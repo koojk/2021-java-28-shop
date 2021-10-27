7483c75893358285dfea0ae3d273d2c57eb99f18
koojk <kljh0881@gmail.com>
2021-10-27 14:50:38 +0900

    link-iteration 완료

--- modules/link-init.js
+++ modules/link-init.js
@@ -1,10 +1,19 @@
 module.exports = {
   admin: {
     index: [],
-    user: [{ path: '', method: 'GET', name: '회원 리스트' }],
+    main: [
+      { path: '/admin', path2: '/admin', src: 'b0' },
+      { path: '/admin/user', path2: '/admin/user', src: 'b1' },
+      { path: '/admin/prd', path2: '/admin/prd', src: 'b2' },
+      { path: '/admin/order', path2: '/admin/order', src: 'b3' },
+      { path: '/admin/board/init', path2: '/admin/board', src: 'b4' },
+      { path: '/admin/system', path2: '/admin/system', src: 'b6' },
+      { path: '//anaytics.google.com', path2: '/admin/analytics', src: 'b9' },
+    ],
+    user: [{ path: '', name: '회원 리스트' }],
     board: [
-      { path: 'init', method: 'GET', name: '게시판 관리' },
-      { path: '', method: 'GET', name: '게시물 관리' },
+      { path: 'init', name: '게시판 관리' },
+      { path: '', name: '게시물 관리' },
     ],
   },
 };
