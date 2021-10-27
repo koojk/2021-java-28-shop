509e669df38c7b282cfbae53d3409cef7826a6e6
koojk <kljh0881@gmail.com>
2021-10-27 15:15:55 +0900

    네비게이션 완료

--- modules/link-init.js
+++ modules/link-init.js
@@ -8,9 +8,19 @@ module.exports = {
       { path: '/admin/order', path2: '/admin/order', src: 'b3' },
       { path: '/admin/board/init', path2: '/admin/board', src: 'b4' },
       { path: '/admin/system', path2: '/admin/system', src: 'b6' },
-      { path: '//anaytics.google.com', path2: '/admin/analytics', src: 'b9' },
+      {
+        path: '//analytics.google.com',
+        path2: '/admin/analytics',
+        src: 'b9',
+        target: '_blank',
+      },
     ],
-    user: [{ path: '', name: '회원 리스트' }],
+    user: [{ path: '', name: '회원 관리' }],
+    order: [{ path: '', name: '주문 관리' }],
+    prd: [
+      { path: '', name: '상품 관리' },
+      { path: '/cate', name: '카테고리 관리' },
+    ],
     board: [
       { path: 'init', name: '게시판 관리' },
       { path: '', name: '게시물 관리' },
