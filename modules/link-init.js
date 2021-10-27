8ee9e757bce49e1d0614a64b0067f80b456e055b
koojk <kljh0881@gmail.com>
2021-10-27 18:08:00 +0900

    admin layout 디자인 완료

--- modules/link-init.js
+++ modules/link-init.js
@@ -3,11 +3,27 @@ module.exports = {
     index: [],
     main: [
       { path: '/admin', path2: '/admin', src: 'b0' },
-      { path: '/admin/user', path2: '/admin/user', src: 'b1' },
+      {
+        path: '/admin/user',
+        path2: '/admin/user',
+        src: 'b1',
+      },
       { path: '/admin/prd', path2: '/admin/prd', src: 'b2' },
-      { path: '/admin/order', path2: '/admin/order', src: 'b3' },
-      { path: '/admin/board/init', path2: '/admin/board', src: 'b4' },
-      { path: '/admin/system', path2: '/admin/system', src: 'b6' },
+      {
+        path: '/admin/order',
+        path2: '/admin/order',
+        src: 'b3',
+      },
+      {
+        path: '/admin/board/init',
+        path2: '/admin/board',
+        src: 'b4',
+      },
+      {
+        path: '/admin/system',
+        path2: '/admin/system',
+        src: 'b6',
+      },
       {
         path: '//analytics.google.com',
         path2: '/admin/analytics',
