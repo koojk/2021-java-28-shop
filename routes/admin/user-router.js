a2c5138a1ab7d377e93066d7b9fefb28ddaae4ca
koojk <kljh0881@gmail.com>
2021-10-28 10:28:35 +0900

    user-update 작업중

--- routes/admin/user-router.js
+++ routes/admin/user-router.js
@@ -1,22 +1,26 @@
-const path = require('path');
-const express = require('express');
-const router = express.Router();
-const { error } = require('../../modules/util');
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const { error, telNumber } = require('../../modules/util')
 
+// 회원리스트
 router.get('/', (req, res, next) => {
-  res.render('admin/user/user-list', { css: 'admin-user' });
-});
+  res.render('admin/user/user-list', { css: 'admin-user' })
+})
 
-router.post('/', (req, res, next) => {
-  res.send('/admin/user:POST');
-});
+// 회원 수정 화면
+router.get('/:id', (req, res, next) => {
+  res.render('admin/user/user-update', { css: 'admin-user', telNumber })
+})
 
+// 회원 수정
 router.put('/', (req, res, next) => {
-  res.send('/admin/user:PUT');
-});
+  res.send('/admin/user:PUT')
+})
 
+// 회원 삭제
 router.delete('/', (req, res, next) => {
-  res.send('/admin/user:DELETE');
-});
+  res.send('/admin/user:DELETE')
+})
 
-module.exports = { name: '/user', router };
+module.exports = { name: '/user', router }
