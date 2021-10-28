7c8edf5569fe125bdfa28a6e4e9fb2b19c91a085
koojk <kljh0881@gmail.com>
2021-10-28 15:05:09 +0900

    tab/header/view-gallery/board-view.ejs

--- middlewares/locals-mw.js
+++ middlewares/locals-mw.js
@@ -1,19 +1,25 @@
+const path = require('path')
 const linkInit = require('../modules/link-init')
+
 module.exports = (req, res, next) => {
   res.locals.user = req.user || null
-  res.locals.links = linkInit.admin
-  res.locals.currentPath = req.originalUrl
-  console.log(req.originalUrl)
-  const currentPaths = req.originalUrl.split('/')
-  currentPaths.shift()
-  res.locals.currentPaths = currentPaths
-  res.locals.secondPath =
-    '/' + currentPaths[0] + (currentPaths[1] ? '/' + currentPaths[1] : '')
+  res.locals.path = {}
+  res.locals.path.init = linkInit.admin
+  res.locals.path.current = req.path
+  res.locals.path.currents = req.path.split('/')
+  res.locals.path.currents.shift()
+  res.locals.path.second = '/' + res.locals.path.currents[0]
+  res.locals.path.second += res.locals.path.currents[1]
+    ? '/' + res.locals.path.currents[1]
+    : ''
+  console.log(res.locals.path.current, res.locals.path.second)
   next()
 }
 
 /* 
-currentPath = 현재경로 originalUrl
-currentPaths = 현재경로 배열
-secondPath = /admin/board
+path : {
+  current: 현재경로
+  currents: 경로 배열
+  second: 2depth 경로
+}
 */
--- modules/link-init.js
+++ modules/link-init.js
@@ -31,15 +31,15 @@ module.exports = {
         target: '_blank',
       },
     ],
-    user: [{ path: '', name: '회원 관리' }],
-    order: [{ path: '', name: '주문 관리' }],
+    user: [{ path: '', path2: '/admin/user', name: '회원 관리' }],
+    order: [{ path: '', path2: '/admin/order', name: '주문 관리' }],
     prd: [
-      { path: '', name: '상품 관리' },
-      { path: 'cate', name: '카테고리 관리' },
+      { path: '', path2: '/admin/prd', name: '상품 관리' },
+      { path: 'cate', path2: '/admin/prd/cate', name: '카테고리 관리' },
     ],
     board: [
-      { path: 'init', name: '게시판 관리' },
-      { path: '', name: '게시물 관리' },
+      { path: 'init', path2: '/admin/board/init', name: '게시판 관리' },
+      { path: '', path2: '/admin/board', name: '게시물 관리' },
     ],
   },
 }
