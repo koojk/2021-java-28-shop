a1757eb42232279cc71accd2a15e7ab297fe1e26
koojk <kljh0881@gmail.com>
2021-10-29 14:12:11 +0900

    prd-form

--- middlewares/locals-mw.js
+++ middlewares/locals-mw.js
@@ -2,14 +2,13 @@ const linkInit = require('../modules/link-init')
 
 module.exports = (req, res, next) => {
   res.locals.user = req.user || null
-  res.locals.path = {}
-  res.locals.path.init = linkInit.admin
-  res.locals.path.current = req.path
-  res.locals.path.currents = req.path.split('/')
-  res.locals.path.currents.shift()
-  res.locals.path.second = '/' + res.locals.path.currents[0]
-  res.locals.path.second += res.locals.path.currents[1]
-    ? '/' + res.locals.path.currents[1]
+  res.locals.init = linkInit.admin
+  res.locals.current = req.path
+  res.locals.currents = req.path.split('/')
+  res.locals.currents.shift()
+  res.locals.second = '/' + res.locals.currents[0]
+  res.locals.second += res.locals.currents[1]
+    ? '/' + res.locals.currents[1]
     : ''
   next()
 }
--- routes/admin/prd-router.js
+++ routes/admin/prd-router.js
@@ -5,14 +5,14 @@ const { error } = require('../../modules/util')
 
 router.get('/', (req, res, next) => {
   if (req.query.type === 'create') {
-    res.render('admin/prd/prd-form', { css: 'admin-prd' })
+    res.render('admin/prd/prd-form', { css: 'admin-prd', type: req.query.type })
   } else {
     res.render('admin/prd/prd-list', { css: 'admin-prd' })
   }
 })
 
 router.get('/:id', (req, res, next) => {
-  res.render('admin/prd/prd-form', { css: 'admin-prd' })
+  res.render('admin/prd/prd-form', { css: 'admin-prd', type: 'update' })
 })
 
 router.post('/', (req, res, next) => {
