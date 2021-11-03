15bcc9336149cf63c4088fd90c4878e6b58e93bb
koojk <kljh0881@gmail.com>
2021-11-03 13:19:29 +0900

    binit.js

--- routes/admin/binit-router.js
+++ routes/admin/binit-router.js
@@ -1,23 +1,43 @@
-const path = require('path');
 const express = require('express');
 const router = express.Router();
-const { error } = require('../../modules/util');
+const createError = require('http-errors');
+const { alert } = require('../../modules/util');
+const { BoardInit } = require('../../models');
 
-router.get('/', (req, res, next) => {
-  const ejs = {};
-  res.render('admin/binit/board-init', ejs);
+router.get('/', async (req, res, next) => {
+  try {
+    const boards = await BoardInit.findAll({ order: [['title', 'asc']] });
+    res.render('admin/binit/board-init', { boards });
+  } catch (err) {
+    next(createError(err));
+  }
 });
 
-router.put('/', (req, res, next) => {
-  res.send('/admin/binit:PUT');
+router.post('/', async (req, res, next) => {
+  try {
+    await BoardInit.create(req.body);
+    res.send(alert('게시판이 생성되었습니다.', '/admin/binit'));
+  } catch (err) {
+    next(createError(err));
+  }
 });
 
-router.delete('/', (req, res, next) => {
-  res.send('/admin/binit:DELETE');
+router.put('/', async (req, res, next) => {
+  try {
+    await BoardInit.update(req.body, { where: { id: req.body.id } });
+    res.send(alert('게시판이 수정되었습니다.', '/admin/binit'));
+  } catch (err) {
+    next(createError(err));
+  }
 });
 
-router.post('/', (req, res, next) => {
-  res.send('/admin/binit:POST');
+router.delete('/', async (req, res, next) => {
+  try {
+    await BoardInit.destroy({ where: { id: req.body.id } });
+    res.send(alert('게시판이 삭제되었습니다.', '/admin/binit'));
+  } catch (err) {
+    next(createError(err));
+  }
 });
 
 module.exports = { name: '/binit', router };
