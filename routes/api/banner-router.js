d682927a1ad3feaf35611d9a732f7d0c2ffbba50
koojk <kljh0881@gmail.com>
2021-12-02 10:35:43 +0900

    Model Board의 getList 수정

---
+++ routes/api/banner-router.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs-extra');
+const express = require('express');
+const { Op } = require('sequelize');
+const router = express.Router();
+const { Board, BoardFile } = require('../../models');
+const boardInit = require('../../middlewares/boardinit-mw');
+
+router.get(
+  '/',
+  (req, res, next) => {
+    req.boardId = 5;
+    next();
+  },
+  boardInit(),
+  async (req, res, next) => {
+    try {
+      const { lists, pager } = await Board.getList('241', null, BoardFile);
+      console.log('======');
+      console.log(lists[0]);
+      res.status(200).json({ list: lists[0] });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+);
+
+module.exports = { name: '/banner', router };
